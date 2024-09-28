a0a1250ce2c80f941d4b4b2018dd674da9523474
Imre Kovács <kovacs.imre1119@gmail.com>
2024-09-28 16:11:12 +0200

    Második feladat megoldás

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Page } from '../../components/page/Page';
+
+const NotFoundPage = () => {
+  return (
+    <Page noCard>
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>404 - Page Not Found</h1>
+        <p>The page you are looking for does not exist. :(</p>
+      </div>
+    </Page>
+  );
+};
+
+export default NotFoundPage;
