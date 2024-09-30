46f752362b866d6f0ba50758c0ee4b25d7c02206
Imre Kovács <kovacs.imre1119@gmail.com>
2024-09-30 16:14:18 +0200

    Bootstrap classok hozzáadva

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Page } from '../../components/page/Page';
+import classNames from 'classnames';
 
 const NotFoundPage = () => {
   return (
     <Page noCard>
-      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <div className='text-center mt-5'>
         <h1>404 - Page Not Found</h1>
         <p>The page you are looking for does not exist. :(</p>
       </div>
