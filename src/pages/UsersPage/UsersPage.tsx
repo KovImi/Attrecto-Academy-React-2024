46a50a25ad54c0c4b3f60f97dd2bb3d42c826c4b
Imre Kovács <kovacs.imre1119@gmail.com>
2024-10-13 17:17:49 +0200

    4. szorgalmi feladat megoldás

--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useEffect, useState } from "react";
-
 import { Page } from "../../components/page/Page";
 import { UserModel } from "../../models/user.model";
 import { userService } from "../../services/user.service";
@@ -8,11 +7,11 @@ import { Link, useNavigate } from "react-router-dom";
 import classNames from "classnames";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-
 import classes from "./UsersPage.module.scss";
 
 const UsersPage = () => {
   const [users, setUsers] = useState<UserModel[]>([]);
+  const [viewType, setViewType] = useState<"card" | "table">("card");
   const navigate = useNavigate();
 
   const fetchUsers = useCallback(async () => {
@@ -25,49 +24,113 @@ const UsersPage = () => {
 
   const goToUserPage = () => {
     navigate("/user");
-  }
+  };
 
   const handleDeleteUser = async (id: string | number) => {
     await userService.deleteUser(id);
-
     fetchUsers();
   };
 
   return (
     <Page title="Users">
-      <div className="row">
+      <div className="row d-flex justify-content-between">
         <div className="col-12 col-sm-6 col-md-4 col-lg-3">
           <Button color="primary" className="w-100 mb-3" onClick={goToUserPage}>
             Create User
           </Button>
         </div>
-      </div>
-      <div className="row">
-        {users.map(({ id, name, image }) => (
-          <div className="col-12 col-sm-6 col-md-4 col-lg-3 my-1">
-            <Link
-              to={`/user/${id}`}
-              className={classNames("card", classes.UserCard)}
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3 ">
+          <div>
+            <Button
+              color="secondary"
+              className="w-100 mb-3"
+              onClick={() => setViewType("card")}
             >
-              <img
-                src={image}
-                alt={`user #${id}`}
-                className={classNames("card-img-top", classes.UserImage)}
-              />
-              <div className="card-body">
-                <h5>{name}</h5>
-              </div>
-              <Button className={classes.DeleteIcon} onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                handleDeleteUser(id)
-              }}>
-                <FontAwesomeIcon icon={faTrash} />
-              </Button>
-            </Link>
+              Card View
+            </Button>
           </div>
-        ))}
+          <div>
+            <Button
+              color="secondary"
+              className="w-100 mb-3"
+              onClick={() => setViewType("table")}
+            >
+              Table View
+            </Button>
+          </div>
+        </div>
+
       </div>
+      {viewType === "card" ? (
+        <div className="row">
+          {users.map(({ id, name, image }) => (
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3 my-1" key={id}>
+              <Link
+                to={`/user/${id}`}
+                className={classNames("card", classes.UserCard)}
+              >
+                <img
+                  src={image}
+                  alt={`user #${id}`}
+                  className={classNames("card-img-top", classes.UserImage)}
+                />
+                <div className="card-body">
+                  <h5>{name}</h5>
+                </div>
+                <Button
+                  className={classes.DeleteIcon}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    handleDeleteUser(id);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faTrash} />
+                </Button>
+              </Link>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(({ id, name, image }) => (
+              <tr key={id}>
+                <td className="align-middle">{id}</td>
+                <td className="align-middle">{name}</td>
+                <td className="align-middle">
+                  <img
+                    src={image}
+                    alt={`user #${id}`}
+                    className={classes.UserImage}
+                    style={{ width: "50px", height: "50px" }}
+                  />
+                </td>
+                <td className="align-middle">
+                    <Button
+                    className={classNames(classes.DeleteIcon)}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      handleDeleteUser(id);
+                    }}
+                    >
+                    <FontAwesomeIcon icon={faTrash} />
+                    </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </Page>
   );
 };
